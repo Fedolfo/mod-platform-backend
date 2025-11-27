222dc79b9d113c80bff8cdd0092fac71c1b02540
Kerlon Amaral <ubuntu.onion@hotmail.com>
2025-11-27 15:05:22 -0300

    Clean up entity exports in index.ts
    Removed comments and streamlined entity exports.

--- src/database/entities/index.ts
+++ src/database/entities/index.ts
@@ -1,10 +1,5 @@
 import { Product } from '../../modules/products/entities/product.entity';
 
-/**
- * Exporta todas as entidades do sistema
- * Adicione novas entidades aqui conforme o projeto cresce
- */
 export const entities = [Product];
 
-// Exportações individuais para facilitar imports
 export { Product };
